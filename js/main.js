8d488d8250491074afcce37eca8548a3e560de1a
MichaelBryant <mikeakakobe@gmail.com>
2017-03-14 20:52:56 -0400

    fix horizontal navbar add js file insert internal js to home index.html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var name = "Michael";
+
+  function checkUser (username) {
+    if (username == "Michael"){
+      console.log("Welcome, Authenticated user.");
+    }
+    else {
+      console.log("Sorry!");
+    }
+  }
+
+  checkUser(name)
