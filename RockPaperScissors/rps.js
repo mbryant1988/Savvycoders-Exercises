764753a5f82dee0ed0d32a4f82c47f8291843048
Michael Bryant <mikeakakobe@gmail.com>
2017-03-16 23:41:51 -0400

    rock paper scissors page

---
+++ RockPaperScissors/rps.js
@@ -0,0 +1,45 @@
+
+var userWins = "Winner Winner Chicken Dinner";
+var computerWins = "LUUUHZZER!";
+var roundCounter = 0;
+
+var gameRound = function(){
+    var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
+    var computerChoice = "R";
+    var rng = Math.random();
+
+    if(rng > 0.66) {
+        computerChoice = "P";
+    } else if (rng > 0.33) {
+        computerChoice = "S";
+    }
+
+    if(computerChoice !== userChoice){
+        if(computerChoice === "R"){
+            if (userChoice === "S") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        } else if (computerChoice === "P") {
+            if( userChoice === "R") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        } else {
+            if( userChoice === "P") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        }
+    } else {
+        alert('Tie!');
+    }
+};
+
+while (roundCounter < 5) {
+    gameRound();
+    roundCounter++;
+}
